6f73b237ecce2a7958c062be98cd084df8b770a4
JyXt <im.jyjyxt@gmail.com>
2023-12-25 21:48:20 +1100

    For updates

--- main.go
+++ main.go
@@ -54,66 +54,69 @@ func main() {
 		panic(err)
 	}
 
-	updates := bot.GetUpdatesChan(u)
-	for update := range updates {
-		if imsg := update.ChannelPost; imsg != nil { // If we got a message
-			var text string
-			cmd := strings.ToLower(strings.TrimSpace(imsg.Text))
-			switch cmd {
-			case "help":
-				cs, err := models.ReadPerpetualCategory(ctx)
-				if err != nil {
-					return
-				}
-				text = strings.Join(cs, ", ")
-			case "low":
-				ps, err := models.ReadLowPerpetuals(ctx)
-				if err != nil {
-					return
-				}
-				text = models.PerpetualsForHuman(ctx, ps)
-			case "week":
-				week, _ := models.ReadWeekStrategies(ctx, models.StrategyNameWeek, 10)
-				var symbols []string
-				for _, s := range week {
-					symbols = append(symbols, s.Symbol)
-				}
-				ps, _ := models.ReadPerpetualsBySymbols(ctx, symbols)
-				text = models.PerpetualsForHuman(ctx, ps)
-			case "buy", "sell":
-				ps, err := models.ReadBestPerpetuals(ctx, cmd)
-				if err != nil {
-					return
-				}
-				text = models.PerpetualsForHuman(ctx, ps)
-			case "go":
-				text = models.Notify(ctx)
-			default:
-				ps, err := models.ReadPerpetualsByCategory(ctx, cmd)
-				if err != nil {
-					return
-				}
-				text = models.PerpetualsForHuman(ctx, ps)
-				if len(ps) == 0 {
-					t := strings.ToUpper(cmd)
-					if f := filters[t+"USDT"]; f != nil {
-						text = models.PerpetualsForHuman(ctx, []*models.Perpetual{f})
+	for {
+		log.Println("bot.GetUpdatesChan starting")
+		updates := bot.GetUpdatesChan(u)
+		for update := range updates {
+			if imsg := update.ChannelPost; imsg != nil { // If we got a message
+				var text string
+				cmd := strings.ToLower(strings.TrimSpace(imsg.Text))
+				switch cmd {
+				case "help":
+					cs, err := models.ReadPerpetualCategory(ctx)
+					if err != nil {
+						return
 					}
-					if f := filters["1000"+t+"USDT"]; f != nil {
-						text = models.PerpetualsForHuman(ctx, []*models.Perpetual{f})
+					text = strings.Join(cs, ", ")
+				case "low":
+					ps, err := models.ReadLowPerpetuals(ctx)
+					if err != nil {
+						return
+					}
+					text = models.PerpetualsForHuman(ctx, ps)
+				case "week":
+					week, _ := models.ReadWeekStrategies(ctx, models.StrategyNameWeek, 10)
+					var symbols []string
+					for _, s := range week {
+						symbols = append(symbols, s.Symbol)
+					}
+					ps, _ := models.ReadPerpetualsBySymbols(ctx, symbols)
+					text = models.PerpetualsForHuman(ctx, ps)
+				case "buy", "sell":
+					ps, err := models.ReadBestPerpetuals(ctx, cmd)
+					if err != nil {
+						return
+					}
+					text = models.PerpetualsForHuman(ctx, ps)
+				case "go":
+					text = models.Notify(ctx)
+				default:
+					ps, err := models.ReadPerpetualsByCategory(ctx, cmd)
+					if err != nil {
+						return
+					}
+					text = models.PerpetualsForHuman(ctx, ps)
+					if len(ps) == 0 {
+						t := strings.ToUpper(cmd)
+						if f := filters[t+"USDT"]; f != nil {
+							text = models.PerpetualsForHuman(ctx, []*models.Perpetual{f})
+						}
+						if f := filters["1000"+t+"USDT"]; f != nil {
+							text = models.PerpetualsForHuman(ctx, []*models.Perpetual{f})
+						}
+					}
+					if text == "" {
+						text = imsg.Text + " 🤟"
 					}
 				}
 				if text == "" {
-					text = imsg.Text + " 🤟"
+					continue
 				}
-			}
-			if text == "" {
-				continue
-			}
-			msg := tgbotapi.NewMessage(imsg.Chat.ID, text)
-			msg.ReplyToMessageID = imsg.MessageID
+				msg := tgbotapi.NewMessage(imsg.Chat.ID, text)
+				msg.ReplyToMessageID = imsg.MessageID
 
-			bot.Send(msg)
+				bot.Send(msg)
+			}
 		}
 	}
 }
