f006111de973755dfebfa8a2c65415fb6e106bee
JyXt <im.jyjyxt@gmail.com>
2024-07-13 23:42:04 +1000

    Fix path error

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState, ReactNode } from "react"
+import { usePathname } from "next/navigation";
 import SideBar from '@/components/Aside'
 import Header from '@/components/Header'
 
@@ -9,12 +10,16 @@ export default function DefaultLayout({
   children: React.ReactNode;
 }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  let pathname = usePathname();
+  if (pathname == '/') {
+    pathname = 'days3-low-up' 
+  }
 
   return (
     <div className="flex h-screen overflow-hidden">
       <SideBar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
       <div className="p-2 sm:ml-64 overflow-y-auto overflow-x-h">
-        <Header slug="days3-low-up" sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}  />
+        <Header slug={pathname.replaceAll('/trends/', '')} sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}  />
         {children}
       </div>
     </div>
